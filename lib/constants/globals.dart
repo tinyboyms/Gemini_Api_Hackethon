bf387c7fa286deefc6fe0111aa732dd17bd7a8a6
tinyboyms <sej9662@gmail.com>
2024-08-12 16:36:59 +0530

    final Commit

---
+++ lib/constants/globals.dart
@@ -0,0 +1,113 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:quize/models/animal.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Globals {
+  Globals._();
+
+  static String get googleAIStudioAPIKey {
+    return dotenv.env['API_KEY'] ?? 'DEFAULT_API_KEY'; // Replace with a sensible default
+  }
+
+  /// animals
+    static final animal = <Animal>[
+      Animal(
+        name: 'cat',
+        color: Colors.white10,
+        lottie: lottieCat,
+      ),
+      Animal(
+        name: 'dog',
+        color: Colors.yellow.shade700,
+        lottie: lottieDog,
+      ),
+      Animal(
+        name: 'elephant',
+        color: Colors.blueGrey.shade700,
+        lottie: lottieElephant,
+      ),
+      Animal(
+        name: 'giraffe',
+        color: Colors.orangeAccent,
+        lottie: lottieGiraffe,
+      ),
+      Animal(
+        name: 'koala',
+        color: Colors.blueGrey,
+        lottie: lottieKoala,
+      ),
+      Animal(
+        name: 'lion',
+        color: Colors.green.shade700,
+        lottie: lottieLion,
+      ),
+      Animal(
+        name: 'monkey',
+        color: Colors.purple.shade900,
+        lottie: lottieMonkey,
+      ),
+      Animal(
+        name: 'tiger',
+        color: Colors.black87,
+        lottie: lottieTiger,
+      )
+    ];
+
+  ///Lottie Files -Animal
+  static final lottieCat = Lottie.asset(
+    'assets/animal/cat.json',
+    width: 300,
+    height: 300,
+  );
+
+  static final lottieDog = Lottie.asset(
+    'assets/animal/dog.json',
+    width: 300,
+    height: 300,
+  );
+
+  static final lottieElephant = Lottie.asset(
+    'assets/animal/elephant.json',
+    width: 300,
+    height: 300,
+  );
+
+  static final lottieGiraffe = Lottie.asset(
+    'assets/animal/giraffe.json',
+    width: 300,
+    height: 300,
+  );
+
+  static final lottieKoala = Lottie.asset(
+    'assets/animal/koala.json',
+    width: 300,
+    height: 300,
+  );
+
+  static final lottieLion = Lottie.asset(
+    'assets/animal/lion.json',
+    width: 300,
+    height: 300,
+  );
+
+  static final lottieMonkey = Lottie.asset(
+    'assets/animal/monkey.json',
+    width: 300,
+    height: 300,
+  );
+
+  static final lottieTiger = Lottie.asset(
+    'assets/animal/tiger.json',
+    width: 300,
+    height: 300,
+  );
+
+  static final lottieLoading = Lottie.asset(
+    'assets/animal/loading.json',
+    width: 300,
+    height: 300,
+  );
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
 
-  Gemini.init(
-    apiKey: Globals.googleAIStudioAPIKey,
-    enableDebugging: true,
-  );
+   Gemini.init(
+      apiKey:Globals.googleAIStudioAPIKey,
+      enableDebugging: true,
+    );
 
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
